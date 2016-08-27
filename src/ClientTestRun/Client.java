0acbae2dd9395f1317d59fcc7380c2fa92a024f6
rohansampson <rohansampson@yahoo.co.in>
2016-08-27 14:01:39 +0530

    Initial commit

---
+++ src/ClientTestRun/Client.java
@@ -0,0 +1,32 @@
+package ClientTestRun;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by Rohan Sampson on 8/23/2016.
+ */
+public class Client {
+    public static void main(String[] args) throws IOException {
+        Socket socket = null;
+        String host = "127.0.0.1";
+
+        socket = new Socket(host, 4444);
+
+        File file = new File("C_file.txt");
+        // Get the size of the file
+        long length = file.length();
+        byte[] bytes = new byte[16 * 1024];
+        InputStream in = new FileInputStream(file);
+        OutputStream out = socket.getOutputStream();
+
+        int count;
+        while ((count = in.read(bytes)) > 0) {
+            out.write(bytes, 0, count);
+        }
+
+        out.close();
+        in.close();
+        socket.close();
+    }
+}
